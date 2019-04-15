cbbadf5618593be58b4856369d1429109b94fe07
Fred Morrison <fpmorrison@verizon.net>
2019-04-15 13:47:01 -0400

    Update DriverPerson.java

--- DriverPerson.java
+++ DriverPerson.java
@@ -7,15 +7,19 @@ public class DriverPerson {
         Person naira = new Student("123456", "Naira", "Ali", "Sophomore", "Information Technology");
         String message = naira.hello();
         message += "\n";
-        message += "I am a " + ( (Student)naira ).getClassLevel();
+        message += "I am a " + ( (Student)naira ).getClassLevel(); // <--- NOTICE THE EXTRA PARENTHESES
         message += "\n";
-        message += "My major is " + ( (Student)naira).getMajor();
+        message += "My major is " + ( (Student)naira ).getMajor();  // <--- NOTICE THE EXTRA PARENTHESES
         message += "\n";
         message += naira.goodbye();
         JOptionPane.showMessageDialog(null, message, "Person Demo", JOptionPane.INFORMATION_MESSAGE);
 
-        Person morrison = new Faculty("987654", "Frederick", "Morrison", "Tutoring", 0);
+        Faculty morrison = new Faculty("987654", "Frederick", "Morrison", "Tutoring", 0);
         message = morrison.hello();
+        message += "\n";
+        message += "My department is " + morrison.getDepartment();
+        message += "\n";
+        message += "I make a salary of " + morrison.getSalary();
         message += "\n";
         message += morrison.goodbye();
         JOptionPane.showMessageDialog(null, message, "Person Demo", JOptionPane.INFORMATION_MESSAGE);
