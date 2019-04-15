8195bec6b789717367a3aa8163d33838a622b4c3
Fred Morrison <fpmorrison@verizon.net>
2019-04-15 08:50:07 -0400

    baseline

---
+++ A.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class A {
+    
+    public void method1(){
+        JOptionPane.showMessageDialog(null, "class A, method1");
+    }
+
+    public void method2(){
+        JOptionPane.showMessageDialog(null, "class A, method2");
+    }
+}
